1d6c92ddfc7693f33504c95a0769a61866c89d36
realfakenerd <lucas210898@hotmail.com>
2025-01-23 17:07:31 -0300

    ✨ refactor: update ReactiveMap and ReactiveSet to use private field for source

--- src/iterators.ts
+++ src/iterators.ts
@@ -6,50 +6,76 @@ export const collectionPrototype = {
 };
 
 export class ReactiveMap<K, V> extends Map<K, V> {
-	private source = new Source(0);
+	#source: Source<number>;
+
+	constructor(values: Iterable<[K, V]>) {
+		super();
+
+		if (values) {
+			for (const [key, value] of values) {
+				this.set(key, value);
+			}
+		}
+
+		this.#source = new Source(0)
+	}
+
 
 	clear() {
 		const hadItems = this.size > 0;
 		super.clear();
-		if (hadItems) this.source?.set(this.source?.get() + 1);
+		if (hadItems) this.#source?.set(this.#source?.get() + 1);
 	}
 
 	delete(key: K) {
 		const result = super.delete(key);
-		if (result) this.source?.set(this.source?.get() + 1);
+		if (result) this.#source?.set(this.#source?.get() + 1);
 		return result;
 	}
 
 	set(key: K, value: V) {
 		const hasKey = this.has(key);
 		super.set(key, value);
-		if (!hasKey) this.source?.set(this.source?.get() + 1);
+		if (!hasKey) this.#source?.set(this.#source?.get() + 1);
 		return this;
 	}
 
 	get(key: K) {
-		this.source?.get();
+		this.#source?.get();
 		return super.get(key);
 	}
 
 	has(key: K) {
-		this.source?.get();
+		this.#source?.get();
 		return super.has(key);
 	}
 
 	get size() {
-		this.source?.get();
+		this.#source?.get();
 		return super.size;
 	}
 }
 
 export class ReactiveSet<T> extends Set<T> {
-	#source = new Source(0);
+	#source: Source<number>;
+
+
+	constructor(values?: Iterable<T>) {
+		super();
+		this.#source = new Source(0);
+
+		if (values) {
+			for (const value of values) {
+				this.add(value);
+			}
+		}
+
+	}
 
 	add(value: T) {
 		const hasValue = this.has(value);
 		super.add(value);
-		if (!hasValue) this.#source.set(this.#source?.get() + 1);
+		if (!hasValue) this.#source.set(this.#source.get() + 1);
 		return this;
 	}
 
