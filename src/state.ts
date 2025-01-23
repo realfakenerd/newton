1607ae825d965f7fd8d98aa31f45b2acf1a50ec3
realfakenerd <lucas210898@hotmail.com>
2025-01-23 18:00:24 -0300

    ✨ refactor: enhance createProxy function with improved handling for arrays and property descriptors

--- src/state.ts
+++ src/state.ts
@@ -3,6 +3,7 @@ import { activeEffect } from "./effect.js";
 import { ReactiveMap, ReactiveSet } from "./iterators.js";
 import { STATE_SYMBOL, UNINITIALIZED } from "./symbols.js";
 import type { Equals, Reaction, Value } from "./types.js";
+import { getDescriptor, isArray } from "./utils.js";
 
 export class Source<T> implements Value<T> {
 	f = 0; // Flags bitmask for internal state tracking
@@ -59,36 +60,57 @@ export class Source<T> implements Value<T> {
 	}
 }
 
-function createProxy<T>(target: T) {
-	if (!target || typeof target !== "object" || STATE_SYMBOL in target) {
-		return target;
+function createProxy<T>(value: T) {
+	if (typeof value !== "object" || value === null || STATE_SYMBOL in value) {
+		return value;
 	}
 
-	if (target instanceof Map) {
-		return new ReactiveMap(target.entries()) as unknown as T;
+	if (value instanceof Map) {
+		return new ReactiveMap(value.entries()) as unknown as T;
 	}
 
-	if (target instanceof Set) {
-		return new ReactiveSet(target.values()) as unknown as T;
+	if (value instanceof Set) {
+		return new ReactiveSet(value.values()) as unknown as T;
 	}
 
 	const sources = new Map<string | symbol, Source<any>>();
+	const isProxiedArray = isArray(value);
 
-	if (Array.isArray(target)) {
+	if (isProxiedArray) {
 		// Create 'length' source eagerly for arrays
-		sources.set("length", new Source((target as unknown[]).length));
+		sources.set("length", new Source((value as unknown[]).length));
 	}
 
-	return new Proxy(target, {
+	return new Proxy(value, {
+		deleteProperty(target, prop) {
+			const source = sources.get(prop);
+			if (source === undefined) {
+				if (prop in target) {
+					sources.set(prop, new Source(UNINITIALIZED));
+				}
+			} else {
+				if (isProxiedArray && typeof prop === 'string') {
+					const ls = sources.get('length') as Source<number>;
+					const index = Number(prop);
+
+					if (Number.isInteger(index) && index >= ls.get()) {
+						ls.set(index);
+					}
+				}
+				source.set(UNINITIALIZED);
+			}
+
+			return true;
+		},
 		get(target, prop, receiver) {
-			if (prop === STATE_SYMBOL) return target;
+			if (prop === STATE_SYMBOL) return value;
 
 			let source = sources.get(prop);
 			const exists = prop in target;
 
 			if (
 				!source &&
-				(!exists || Object.getOwnPropertyDescriptor(target, prop)?.writable)
+				(!exists || getDescriptor(target, prop)?.writable)
 			) {
 				source = new Source(
 					createProxy(
@@ -100,37 +122,107 @@ function createProxy<T>(target: T) {
 				sources.set(prop as string, source);
 			}
 
-			if (source) {
+			if (source !== undefined) {
 				const value = source.get();
 				return value === UNINITIALIZED ? undefined : value;
 			}
 
 			return Reflect.get(target, prop, receiver);
 		},
-		set(target, prop, value) {
-			let source = sources.get(prop as string);
-			const has = prop in target;
+		getOwnPropertyDescriptor(target, prop) {
+			const descriptor = Reflect.getOwnPropertyDescriptor(target, prop);
+
+			if (descriptor && 'value' in descriptor) {
+				const source = sources.get(prop as string);
+
+				if (source) descriptor.value = source.get();
+			} else if (descriptor === undefined) {
+				const source = sources.get(prop);
+				const value = source?.v;
+
+				if (source !== undefined && value !== UNINITIALIZED) {
+					return {
+						enumerable: true,
+						configurable: true,
+						value,
+						writable: true,
+					}
+				}
+			}
+
+			return descriptor;
+		},
+		has(target, prop) {
+			if (prop === STATE_SYMBOL) return true;
+			let source = sources.get(prop);
+			const has = (source !== undefined && source.v !== UNINITIALIZED) || Reflect.has(target, prop);
 
 			if (
-				!source &&
-				(!has || Object.getOwnPropertyDescriptor(target, prop)?.writable)
+				source !== undefined ||
+				(activeEffect !== null && (!has || getDescriptor(target, prop)?.writable))
 			) {
-				source = new Source(undefined);
-				sources.set(prop, source);
+				if (source === undefined) {
+					source = new Source(
+						has ?
+							createProxy((target as Record<string, unknown>)[prop as string])
+							: UNINITIALIZED
+					);
+
+					sources.set(prop, source);
+				}
+
+				const value = source.get();
+
+				if (value === UNINITIALIZED) {
+					return false;
+				}
 			}
 
-			if (source) {
-				source.set(createProxy(value));
-			}
+			return has;
 
-			if (Array.isArray(target) && prop === "length") {
-				for (let i = value; i < source?.v; i++) {
-					let otherSource = sources.get(i);
+		},
+		set(target, prop, value, receiver) {
+			let source = sources.get(prop as string);
+			let has = prop in target;
+
+			if (isProxiedArray && prop === "length") {
+				const length = (source as Source<number>).v;
+				for (let i = value; i < length; i++) {
+					let otherSource = sources.get(String(i));
 					if (otherSource !== undefined) {
 						otherSource.set(undefined);
 					} else if (i in target) {
 						otherSource = new Source(UNINITIALIZED);
-						sources.set(i, otherSource)
+						sources.set(String(i), otherSource)
+					}
+				}
+			}
+
+			if (
+				!source &&
+				(!has || getDescriptor(target, prop)?.writable)
+			) {
+				source = new Source(undefined);
+				sources.set(prop, source);
+			} else {
+				has = source?.v !== UNINITIALIZED;
+				source?.set(createProxy(value));
+			}
+
+			const descriptor = Reflect.getOwnPropertyDescriptor(target, prop);
+
+			if (descriptor?.set) {
+				descriptor.set.call(target, receiver);
+			}
+
+			if (!has) {
+				if (isProxiedArray && typeof prop === 'string') {
+					const index = Number(prop);
+					if (Number.isInteger(index)) {
+						const ls = sources.get('length') as Source<number>;
+						if (index >= ls.get()) {
+							ls.set(index + 1);
+						}
 					}
 				}
 			}
