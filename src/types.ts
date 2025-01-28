1a636ea621253a1058429ed083714f571ac29aaa
realfakenerd <lucas210898@hotmail.com>
2025-01-28 18:05:39 -0300

    ✨ feat: refine type definitions for Effect and DerivedValue interfaces

--- src/types.ts
+++ src/types.ts
@@ -1,36 +1,37 @@
 export type Equals<T> = (a: T, b: T) => boolean;
 
-export type EffectFn = (() => void) | (() => Promise<void>);
+export type EffectFn = () => void;
 
 export interface Signal {
-	/** Flags bitmask */
-	f: number;
+	f: number; // flags
+	wv: number; // write version
 }
 
 export interface Value<V = unknown> extends Signal {
-	v: V;
-	reactions: Reaction[] | null;
+	v: V; // value
+	rv: number; // read version
 	equals: Equals<V>;
+	reactions: Reaction[] | null;
 }
 
 export interface Reaction extends Signal {
-	flags: number;
 	fn: EffectFn | null;
 	deps: Value[] | null;
 }
 
-export interface DerivedValue<T = any> {
-	flags: number;
-	writeVersion: number;
-	readVersion: number;
-	reactions: Reaction[] | null;
-	deps: Value[] | null;
-	value: T;
-	fn: () => T;
-	equals: Equals<T>;
-	computeCount: number;
-	lastComputedTime: number;
-	cacheThreshold: number;
-	parent: DerivedValue<any> | null;
-	children: DerivedValue<any>[] | null;
+export interface Derived<V = unknown> extends Value<V>, Reaction {
+	fn: () => V;
+	parent: Derived | null;
+	children: null | Reaction[];
+}
+
+export interface Effect extends Reaction {
+	f: number; // flags
+	prev: Effect | null;
+	next: Effect | null;
+	last: Effect | null;
+	first: Effect | null;
+	parent: Effect | null;
+	deriveds: Derived[] | null;
+	teardown: EffectFn | null;
 }
