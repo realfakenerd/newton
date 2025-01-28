a9d4731f64c7ac1a007dde74c24668633021bd19
realfakenerd <lucas210898@hotmail.com>
2025-01-28 18:05:32 -0300

    ✨ feat: add utility functions for object and array prototype access

--- src/utils.ts
+++ src/utils.ts
@@ -1,2 +1,7 @@
-export const isArray = Array.isArray;
+export const objectPrototype = Object.prototype;
 export const getDescriptor = Object.getOwnPropertyDescriptor;
+export const getPrototypeOf = Object.getPrototypeOf;
+
+export const arrayPrototype = Array.prototype;
+export const isArray = Array.isArray;
+export const indexOf = arrayPrototype.indexOf;
